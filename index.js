c82e67d638cc688be76f56f2b6c9669ed9af072e
Albert Cheruiyot <bettalbert99@gmail.com>
2024-03-31 22:22:36 +0300

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+  function superbowlWin(record){
+    // I used find() method to find a winning entry in the record
+    var year = record.find(function(entry){
+        // I checked if the result property of the current entry is strictly "W"
+        return entry.result === "W"
+    })
+    // if win eturns year property otherwise return undefined
+    return year ? year.year : undefined;
+  }
